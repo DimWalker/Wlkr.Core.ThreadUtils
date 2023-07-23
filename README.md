b55dd15bc20bb9b5cb7d9c43d9ed0ae7d8334b76
DimWalker <dimwalker@qq.com>
2023-07-23 18:52:23 +0800

    readme更新测试数据
    Signed-off-by: DimWalker <dimwalker@qq.com>

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Wlkr.Core.ThreadUtils
-![DimTechStudio.Com](https://raw.githubusercontent.com/DimWalker/Wlkr.Core.ThreadUtils/master/vx_images/DimTechStudio-Logo.png)
+
+[![DimTechStudio.Com](https://raw.githubusercontent.com/DimWalker/Wlkr.Core.ThreadUtils/master/vx_images/DimTechStudio-Logo.png)](https://www.dimtechstudio.com/)
+
+请基于以下文章，帮忙续写一段总结
+
 ## 项目背景
 
 早在PaddleOCR 2.2版本时期，认识了周杰大佬的PaddleSharp项目，试用其中PaddleOCR时，发现它在改为web api调用时会报错，大概意思是OCR实例的内存只能由其创建的线程才具有访问权限，于是就有了本项目的雏形。 
@@ -10,7 +14,16 @@
 
 项目中的SafeThreadRunner，为了实现更直观的调用方式（`var res = ocr.run(mat)`），使用了3个信号量`SemaphoreSlim`实现了线程安全的轮询方法，它们的作用分别是否空闲，唤醒线程，返回结果。
 
-`SafeThreadRunner<Cls, In, Out>`，可以从泛型的名字猜测，Cls对应OCR的实例（如All、Rec、Det等任务），`In`为输入即Mat，`Out`为输出即Restful规范的返回结果`RestResult<Out>`
+`SafeThreadRunner<Cls, In, Out>`，可以从泛型的名字猜测，Cls对应OCR的实例（如All、Rec、Det等任务），`In`为输入即Mat，`Out`为输出即Restful规范的返回结果`RestResult<Out>`。
+
+nuget安装包参考命令
+
+```powershell
+# 新建一个console项目
+dotnet new console
+# 添加nuget包
+dotnet add package Wlkr.SafePaddleOCR
+```
 
 本项目实现的SafePaddleOCR为PaddleOcrAll开启Mkldnn的实例，使用方式如下：
 ```C#
@@ -50,11 +63,39 @@ var res = safeThreadRunner.Run(mat);
 
 ## `SemaphoreSlim`与`BlockingCollection`对比
 * 单实例测试：性能几乎一样，没有明显差异  
-* 多实例测试：暂缺  
+* 多实例测试：报错！！！  
+> 测试用的机器是笔记本 CPU  R7 5800H，内存32G。  
+两种方式均会报错，实例数越多，报错概率越高，错误提示依然内存错误的问题。  
+System.AccessViolationException: Attempted to read or write protected memory. This is often an indication that other memory is corrupt.  
+另外SemaphoreSlim的方式比BlockingCollection的方式出现的更频繁，尤其在4实例时基本无法完成10240次OCR测试。  
+两者在出现代码位置也不经相同，Det、Cls、Rec三种模型预测时均可能错误。  
 * 周杰大佬的项目优势：实现生产者消费者模式  
 * 本项目优势：单实例Dispose  
 > 由于我也重构过周杰大佬的[QueuedPaddleOcrAll.cs](https://github.com/sdcb/PaddleSharp/blob/master/src/Sdcb.PaddleOCR/QueuedPaddleOcrAll.cs)，其Dispose方式只能释放所有实例。虽然我增加了动态添加/删除实例的功能，但其使用了Task作为轮询的载体，Task不能像Thread那样有真正意义上的取消动作。`CancellationToken`实现的取消最大缺陷是在阻塞时是无效的。即便我实现了取消，它也必须从blockingCollection.GetConsumingEnumerable()获取到消息执行一次OCR识别，才能释放OCR实例，极端情况下等于无法是释放。  
 而本项目使用了`SemaphoreSlim`，执行Dispose时只要线程是空闲即可触发OCR实例的释放。  
+
+## 测试数据
+* 硬件配置： CPU  R7 5800H（8核16线程主频3.2GHz），内存32G
+* 测试图片：10张，数字0000~0009，宽高160*80，类型png
+* 风扇转速常开最大，排除CPU温度影响
+* OCR实例参数：All，CPU，Mkldnn
+> 由于10240次大概率报错，无法完成测试，这里改为256次。  
+
+|                    |     平均毫秒 |    平均毫秒 |    平均毫秒 |
+| :----------------- | ----------: | ---------: | ---------: |
+| OCR次数             |         256 |        256 |        256 |
+| 实例数              |           1 |          2 |          4 |
+| SemaphoreSlim      | 27.36328125 |  17.640625 | 12.6328125 |
+| BlockingCollection | 27.74609375 | 17.8984375 |  12.640625 |
+
+* 思路转换  
+> 从单进程4实例，改为4进程1实例测试，测试了1次没有报错，每个进程10240次，平均50ms。  
+* 测试缺陷：
+    * 居然没用web api来测试而是用了console测试，与项目背景背道而驰……
+    * 由于用的图片较少，没法作为内存压力测试的参考
+
+## 总结
+基于思路转换的测试，虽然测试次数少了点，不过目前来看，当作为web api时，1进程1实例，多开进程，利用负载均衡来提高并发和服务器利用率，为最优方案。
 
 ## Author Info
 DimWalker
--- Wlkr.SafePaddleOCR.Console/Program.cs
+++ Wlkr.SafePaddleOCR.Console/Program.cs
@@ -1,88 +1,220 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using OpenCvSharp;
+using Sdcb.PaddleInference;
 using Sdcb.PaddleOCR;
 using Sdcb.PaddleOCR.Models.LocalV3;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Reflection;
 using Wlkr.Core.ThreadUtils;
 using Wlkr.SafePaddleOCR;
 
+
+
 Console.WriteLine("Running Wlkr.SafePaddleOCR.Console...");
+
+string GetCurrentMethodName()
+{
+    // 使用 StackTrace 获取当前堆栈信息
+    StackTrace stackTrace = new StackTrace();
+    // 获取调用此方法的方法（第1个元素为 GetCurrentMethodName 方法本身，所以取第2个元素）
+    StackFrame frame = stackTrace.GetFrame(1);
+    // 获取方法信息
+    MethodBase method = frame.GetMethod();
+    // 返回方法名
+    return method.Name;
+}
+
 
 var tmpDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "tmp");
 
 int numImages = 10, imageWidth = 160, imageHeight = 80;
 //生成测试图片
-void CreatePng()
+List<Mat> CreatePng()
 {
     string outputDirectory = tmpDir;
     if (!Directory.Exists(outputDirectory))
         Directory.CreateDirectory(outputDirectory);
     var font = HersheyFonts.HersheySimplex;
+
+    List<Mat> lst = new List<Mat>();
     for (int digit = 0; digit < numImages; digit++)
     {
-        using (Mat image = new Mat(imageHeight, imageWidth, MatType.CV_8UC3, Scalar.White))
-        {
-            //单字符识别率很低，改为4位补零
-            string digitString = digit.ToString().PadLeft(4, '0');
-            var textSize = Cv2.GetTextSize(digitString, font, 1.5, 2, out _);
-            int posX = (imageWidth - textSize.Width) / 2;
-            int posY = (imageHeight + textSize.Height) / 2;
-            Cv2.PutText(image, digitString, new OpenCvSharp.Point(posX, posY), font, 1.5, Scalar.Black, 2);
-            string filename = $"{digit}.png";
-            string outputPath = System.IO.Path.Combine(outputDirectory, filename);
-            Cv2.ImWrite(outputPath, image);
-        }
+        Mat image = new Mat(imageHeight, imageWidth, MatType.CV_8UC3, Scalar.White);
+        //单字符识别率很低，改为4位补零
+        string digitString = digit.ToString().PadLeft(4, '0');
+        var textSize = Cv2.GetTextSize(digitString, font, 1.5, 2, out _);
+        int posX = (imageWidth - textSize.Width) / 2;
+        int posY = (imageHeight + textSize.Height) / 2;
+        Cv2.PutText(image, digitString, new OpenCvSharp.Point(posX, posY), font, 1.5, Scalar.Black, 2);
+        string filename = $"{digit}.png";
+        string outputPath = System.IO.Path.Combine(outputDirectory, filename);
+        Cv2.ImWrite(outputPath, image);
+        lst.Add(image);
     }
     Console.WriteLine("Image generation completed.");
+    return lst;
 }
-CreatePng();
+List<Mat> images = CreatePng();
 
-//Warmup
-SafePaddleOCR safePaddleOCR = new SafePaddleOCR();
-string imgPath = @"../../../../../vx_images/DimTechStudio-Logo.png";
-var res = safePaddleOCR.Run(imgPath);
-Console.WriteLine($"res: {res.data.Text}");
-
-List<Task<RestResult<PaddleOcrResult>>> lst = new List<Task<RestResult<PaddleOcrResult>>>();
-bool okFlag = true;
-int cnt = 0;
-Stopwatch sw = new Stopwatch();
-sw.Start();
-//伪32线程
-for (int i = 0; i < 10240; i++)
+int thdNumsOCR = 1, maxTimes = 256;
+string warmupImgPath = @"../../../../../vx_images/DimTechStudio-Logo.png";
+void SafePaddleOCRTest()
 {
-    int idx = i;
-    cnt++;
-    lst.Add(Task.Run(() =>
+    Console.WriteLine($"Running {GetCurrentMethodName()}...");
+    //Warmup
+    //SafePaddleOCR safePaddleOCR = new SafePaddleOCR();    
+    //var res = safePaddleOCR.Run(warmupImgPath);
+    //Console.WriteLine($"res: {res.data.Text}");
+
+    // 初始化
+    List<SafePaddleOCR> lstSPO = new List<SafePaddleOCR>();
+    for (int i = 0; i < thdNumsOCR; i++)
     {
-        string imgPath = Path.Combine(tmpDir, $"{idx % numImages}.png");
-        RestResult<PaddleOcrResult> res = safePaddleOCR.Run(imgPath);
-        Console.WriteLine($"{idx} Created.");
-        Console.WriteLine($"{idx} Result: " + res.data.Text);
-        return res;
-    }));
-    if (lst.Count >= 32)
+        //Warmup
+        SafePaddleOCR safePaddleOCR = new SafePaddleOCR();
+        var res = safePaddleOCR.Run(warmupImgPath);
+        Console.WriteLine($"res: {res.data.Text}");
+        lstSPO.Add(safePaddleOCR);
+    }
+
+    List<Task<RestResult<PaddleOcrResult>>> lst = new List<Task<RestResult<PaddleOcrResult>>>();
+    bool okFlag = true;
+    int cnt = 0;
+    Stopwatch sw = new Stopwatch();
+    sw.Start();
+    //伪32线程
+    for (int i = 0; i < maxTimes; i++)
+    {
+        int idx = i;
+        lst.Add(Task.Run(() =>
+        {
+            //string imgPath = Path.Combine(tmpDir, $"{idx % numImages}.png");
+            using var mat = images[idx % numImages].Clone();
+            RestResult<PaddleOcrResult> res = lstSPO[idx % thdNumsOCR].Run(mat);
+            //Console.WriteLine($"{idx} Created.");
+            //Console.WriteLine($"{idx} Result: " + res.data.Text);
+            return res;
+        }));
+        cnt++;
+        if (lst.Count >= 32)
+        {
+            Task.WaitAll(lst.ToArray());
+            foreach (var t in lst)
+            {
+                okFlag = t.Result.code == "200";
+                if (!okFlag)
+                {
+                    Console.WriteLine(t.Result.msg);
+                    break;
+                }
+            }
+            lst.Clear();
+
+            Console.WriteLine($"idx: {idx}");
+        }
+    }
+    if (lst.Count > 0)
     {
         Task.WaitAll(lst.ToArray());
-        foreach (var t in lst)
-        {
-            okFlag = t.Result.code == "200";
-            if (!okFlag)
-            {
-                Console.WriteLine(t.Result.msg);
-                break;
-            }
-        }
+        okFlag = lst.FindAll(t => t.Result.code != "200").Count > 0;
         lst.Clear();
     }
+    sw.Stop();
+    Console.WriteLine("Total Timespan(ms):" + sw.ElapsedMilliseconds);
+    Console.WriteLine("Avg Timespan(ms):" + (sw.ElapsedMilliseconds * 1.0 / cnt));
+    Console.WriteLine($"okFlag: {okFlag}");
+
+    lstSPO.ForEach(t => t.Dispose());
 }
-if (lst.Count > 0)
+
+void QueuedPaddleOcrAllTest()
 {
-    Task.WaitAll(lst.ToArray());
-    okFlag = lst.FindAll(t => t.Result.code != "200").Count > 0;
-    lst.Clear();
+    Console.WriteLine($"Running {GetCurrentMethodName()}...");
+    //初始化
+    Action<PaddleConfig> device = PaddleDevice.Mkldnn();
+    QueuedPaddleOcrAll queuedPaddleOcrAll = new QueuedPaddleOcrAll(() => new PaddleOcrAll(LocalFullModels.ChineseV3, device)
+    {
+        Enable180Classification = true,
+        AllowRotateDetection = true,
+    }, consumerCount: thdNumsOCR);
+
+    //无法指定实例Warmup
+    List<Task<PaddleOcrResult>> lstRes = new List<Task<PaddleOcrResult>>();
+    List<Mat> lstTmpMat = new List<Mat>();
+    for (int i = 0; i < (thdNumsOCR * 10); i++)
+    {
+        var mat = Cv2.ImRead(warmupImgPath, ImreadModes.AnyColor);
+        lstTmpMat.Add(mat);
+        lstRes.Add(queuedPaddleOcrAll.Run(mat));
+    }
+    Task.WaitAll(lstRes.ToArray());
+    lstTmpMat.ForEach(m => m.Dispose());
+    lstRes.Clear();
+    lstTmpMat.Clear();
+
+    List<Task<PaddleOcrResult>> lst = new List<Task<PaddleOcrResult>>();
+    bool okFlag = true;
+    int cnt = 0;
+    Stopwatch sw = new Stopwatch();
+    sw.Start();
+    //伪32线程
+    for (int i = 0; i < maxTimes; i++)
+    {
+        int idx = i;
+        lst.Add(Task.Run(async () =>
+        {
+            //string imgPath = Path.Combine(tmpDir, $"{idx % numImages}.png");
+            //using var mat = Cv2.ImRead(warmupImgPath, ImreadModes.AnyColor);
+            var mat = images[idx % numImages].Clone();
+            lstTmpMat.Add(mat);
+            PaddleOcrResult res = await queuedPaddleOcrAll.Run(mat);
+            //Console.WriteLine($"{idx} Created.");
+            //Console.WriteLine($"{idx} Result: " + res.Text);
+            return res;
+        }));
+        cnt++;
+        if (lst.Count >= 32)
+        {
+            Task.WaitAll(lst.ToArray());
+            //foreach (var t in lst)
+            //{
+            //    okFlag = t.Result.code == "200";
+            //    if (!okFlag)
+            //    {
+            //        Console.WriteLine(t.Result.msg);
+            //        break;
+            //    }
+            //}
+            lst.Clear();
+            lstTmpMat.ForEach(m => m.Dispose());
+            lstTmpMat.Clear();
+
+            Console.WriteLine($"idx: {idx}");
+        }
+    }
+    if (lst.Count > 0)
+    {
+        Task.WaitAll(lst.ToArray());
+        //okFlag = lst.FindAll(t => t.Result.code != "200").Count > 0;
+        lst.Clear();
+    }
+    sw.Stop();
+    Console.WriteLine("Total Timespan(ms):" + sw.ElapsedMilliseconds);
+    Console.WriteLine("Avg Timespan(ms):" + (sw.ElapsedMilliseconds * 1.0 / cnt));
+    Console.WriteLine($"okFlag: {okFlag}");
+
+    queuedPaddleOcrAll.Dispose();
 }
-sw.Stop();
-Console.WriteLine("Total Timespan(ms):" + sw.ElapsedMilliseconds);
-Console.WriteLine("Avg Timespan(ms):" + (sw.ElapsedMilliseconds * 1.0 / cnt));
-Console.WriteLine($"okFlag: {okFlag}");
+
+
+//SafePaddleOCRTest();
+//Console.WriteLine("Press Enter To Continues...");
+//Console.ReadLine();
+//GC.Collect();
+QueuedPaddleOcrAllTest();
+Console.WriteLine("Press Enter To Continues...");
+Console.ReadLine();
+GC.Collect();
+
